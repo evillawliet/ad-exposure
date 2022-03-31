3b09910ef1da21e9d9f1d60c09d4359924986e50
lawliet <evilblue.lawliet@gmail.com>
2022-03-31 11:38:40 +0800

    eslint test

--- demo/index.ts
+++ demo/index.ts
@@ -1,22 +1,10 @@
 import { exposure } from '../dist'
 
 // 要观察的对象
-const dom1 = document.getElementById('expr-1')
 const dom5 = document.getElementById('expr-5')
 
 exposure({
-  ele: [dom1!, dom5!],
-  fallback: () => {
-    console.log('wobaoguang')
-  },
-}, {
-  infinite: true,
-})
-exposure({
   ele: [dom5!],
-  fallback: (e) => {
-    console.log('wobaoguang2', e)
-  },
 }, {
   infinite: true,
   threshold: [0, 0.5, 1],
