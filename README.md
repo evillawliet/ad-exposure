f76007a7f588ae1bc3cb881dcbed145a0e5a6ab5
lawliet <lawlietcos@outlook.com>
2022-03-31 23:53:39 +0800

    fix: ele options

--- README.md
+++ README.md
@@ -2,4 +2,29 @@
 
 [![NPM version](https://img.shields.io/npm/v/ad-exposure?color=a1b858&label=)](https://www.npmjs.com/package/ad-exposure)
 
+> A Function to observe a element
+
+## install
+
+```bash
+npm install ad-exposure
+```
+
+## usage
+
+```js
+import { adExposure } from 'ad-exposure'
+
+adExposure({
+  ele: dom,
+  callback: (entry) => {},
+})
+```
+
+## TODOs
+
+- throw error
+- peerDependencies
+- IntersectionObserver polyfill
+
 ## License
--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,7 @@
-interface ObserverTarget {
-  ele: HTMLElement | HTMLElement[]
-  fallback?: (entry: IntersectionObserverEntry) => void
-}
+import type { ObserverConfig, ObserverTarget } from './types'
 
-interface ObserverConfig extends IntersectionObserverInit {
-  infinite?: boolean
-}
+export { exposure as adExposure } from './observer'
 
-const isArray = Array.isArray
-
-export function exposure(target: ObserverTarget | ObserverTarget[], options?: ObserverConfig): IntersectionObserver | undefined {
-  const weakMap = new WeakMap()
-
-  try {
-    const observer = new IntersectionObserver((entries) => {
-      for (const entry of entries) {
-        if (entry.isIntersecting) {
-          weakMap.get(entry.target)?.(entry)
-          if (!options?.infinite) {
-            observer?.unobserve(entry.target)
-            weakMap.delete(entry.target)
-          }
-        }
-      }
-    }, {
-      root: options?.root || null,
-      threshold: options?.threshold || 0,
-      rootMargin: options?.rootMargin || '0px',
-    })
-
-    if (isArray(target)) {
-      target.forEach((t) => {
-        if (isArray(t.ele)) {
-          t.ele.forEach((e) => {
-            observer.observe(e)
-            weakMap.set(e, t.fallback)
-          })
-        }
-        else {
-          observer.observe(t.ele)
-          weakMap.set(t.ele, t.fallback)
-        }
-      })
-    }
-    else {
-      if (isArray(target.ele)) {
-        target.ele.forEach((e) => {
-          observer.observe(e)
-          weakMap.set(e, target.fallback)
-        })
-      }
-      else {
-        observer.observe(target.ele)
-        weakMap.set(target.ele, target.fallback)
-      }
-    }
-
-    return observer
-  }
-  catch (error) {
-    console.error(error)
-  }
+export {
+  ObserverConfig, ObserverTarget,
 }
